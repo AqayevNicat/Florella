6d1c163568fbf87693a38fed0a674a5d65ad19ce
Your Name <nijatma@code.local>
2022-04-01 17:37:42 +0400

    ssaas

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly FlorellaDbContext _context;
+        public HomeController(FlorellaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products =  await _context.Products.Include(p=>p.Category).ToListAsync();
+            List<Category> categories = await _context.Categories.ToListAsync();
+
+            CategoryProduct categoryProduct = new CategoryProduct
+            {
+                Product = products,
+                Category = categories
+            };
+
+            return View(categoryProduct);
+        }
+    }
+}
---
+++ WebApplication1/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewComponenets
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly FlorellaDbContext _context;
+        public FooterViewComponent(FlorellaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Dictionary<string, string> setting = await _context.Settings.ToDictionaryAsync(x=>x.Key,x=>x.Value);
+            return View(await Task.FromResult(setting));
+        }
+    }
+}
